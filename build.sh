5242400cb05a71b87f4bac8b6ace18ef98eb41c3
vance <vance.8807@gmail.com>
2018-10-26 21:19:32 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+version=0.0.1
+
 mvn clean package
 
 str='#!/bin/sh \n MYSELF=`which "$0" 2>/dev/null` \n [ $? -gt 0 -a -f "$0" ] && MYSELF="./$0" \n java=java \n if test -n "$JAVA_HOME"; then \n java="$JAVA_HOME/bin/java" \n fi \n exec "$java" -Dcli=true $java_args -jar $MYSELF "$@" \n exit 1'
@@ -11,3 +13,9 @@ echo -e $str >> flyermaker
 cat flyer-maker.jar >> flyermaker
 
 chmod +x flyermaker
+
+cp classes/application.properties .
+
+zip -r flyermaker-${version}.zip flyermaker application.properties
+
+tar zcvf flyermaker-${version}.tar.gz flyermaker application.properties
